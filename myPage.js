3a0423b19693114b043eac4ae2817a8c77e78538
sath-wick <81036216+sath-wick@users.noreply.github.com>
2024-08-01 15:58:00 +0530

    Add files via upload

--- myPage.js
+++ myPage.js
@@ -3,23 +3,31 @@ let clickLocation = 0;
 const earth = document.getElementById('sphere');
 let finalX = 0;
 
-earth.addEventListener('mousedown', (e) => {
-    clickLocation = e.clientX;
+function startMove(event){
+    clickLocation = event.clientX;
     isDragging = true;
-});
+}
 
-earth.addEventListener('mousemove', (e) => {
+function moveEarth(event){
     if (isDragging) {
-        let distanceMoved = e.clientX - clickLocation;
+        let distanceMoved = event.clientX - clickLocation;
         earth.style.backgroundPosition = `${finalX + distanceMoved}px,0px`;
-    }   
-});
+    } 
+}
+
+function endMove(){
+    isDragging = false;
+    let current_background_position = earth.style.getPropertyValue('background-position');
+    finalX = parseInt(current_background_position.split('px')[0]);
+    
+}
+
+
+earth.addEventListener('mousedown', startMove);
+earth.addEventListener('touchstart', startMove);
+earth.addEventListener('touchmove', moveEarth);
+earth.addEventListener('mousemove', moveEarth);
 
 // Optional: To handle the case where the mouse is released outside the plane element
-document.addEventListener('mouseup', () => {
-    isDragging = false;
-    let bgpos = earth.style.getPropertyValue('background-position');
-    finalX = parseInt(bgpos.split('px')[0]);
-    console.log(finalX);
-    
-});
+document.addEventListener('mouseup',endMove);
+document.addEventListener('touchend',endMove);
